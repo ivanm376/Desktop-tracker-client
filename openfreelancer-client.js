b57cd7e638596f12178b43c909184ce3415ae533
IvanM <ivan.malyshev.376@gmail.com>
2016-11-20 02:24:38 +0500

    comment

--- openfreelancer-client.js
+++ openfreelancer-client.js
@@ -3,6 +3,7 @@ var request = require('request');
 var fs = require('fs');
 
 (function timer() {
+  // TODO add Mac/Windows command
   exec('import -quality 50 -window root screenshot.jpg', function () {
     fs.createReadStream('screenshot.jpg').pipe(request.post(process.env.URL));
   });
