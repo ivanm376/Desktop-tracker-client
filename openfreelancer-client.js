cff5a9afb9f47da6ee7795b1bbcbbdd28f9f36ae
IvanM <ivan.malyshev.376@gmail.com>
2016-11-20 20:55:09 +0500

    tabs

--- openfreelancer-client.js
+++ openfreelancer-client.js
@@ -3,16 +3,16 @@ var request = require('request');
 var fs = require('fs');
 
 (function timer() {
-  // TODO add Mac/Windows command
-  exec('import -window root -quality 50 screenshot.jpg', function () {
-    fs.createReadStream('screenshot.jpg').pipe(request.post(process.env.URL + '/screehshot'));
-  });
-  exec('import -window root -resize 200 thumbnail.jpg', function () {
-    fs.createReadStream('thumbnail.jpg').pipe(request.post(process.env.URL + '/thumbnail'));
-  });
-  setTimeout(timer, 10 * 60 * 1000); // once 10 minutes
+    // TODO add Mac/Windows command
+    exec('import -window root -quality 50 screenshot.jpg', function () {
+        fs.createReadStream('screenshot.jpg').pipe(request.post(process.env.URL + '/screehshot'));
+    });
+    exec('import -window root -resize 200 thumbnail.jpg', function () {
+        fs.createReadStream('thumbnail.jpg').pipe(request.post(process.env.URL + '/thumbnail'));
+    });
+    setTimeout(timer, 10 * 60 * 1000); // once 10 minutes
 }());
 
 process.on('uncaughtException', function (err) {
-//   console.log('uncaughtException');
+//     console.log('uncaughtException');
 });
