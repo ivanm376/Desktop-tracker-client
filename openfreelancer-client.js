9fdc4fd5f45649cab7269d47357da4b1e4f54788
IvanM <ivan.malyshev.376@gmail.com>
2016-11-21 00:00:46 +0500

    fix

--- openfreelancer-client.js
+++ openfreelancer-client.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 (function timer() {
     // TODO add Mac/Windows command
     exec('import -window root -quality 50 screenshot.jpg', function () {
-        fs.createReadStream('screenshot.jpg').pipe(request.post(process.argv[2] + '/screehshot'));
+        fs.createReadStream('screenshot.jpg').pipe(request.post(process.argv[2] + '/screenshot'));
     });
     exec('import -window root -resize 200 thumbnail.jpg', function () {
         fs.createReadStream('thumbnail.jpg').pipe(request.post(process.argv[2] + '/thumbnail'));
