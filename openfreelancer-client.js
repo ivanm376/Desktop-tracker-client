60ced4219fdc2294683b81a8022e3f33f473fd71
IvanM <ivan.malyshev.376@gmail.com>
2017-01-12 13:57:20 +0300

    es6

--- openfreelancer-client.js
+++ openfreelancer-client.js
@@ -1,18 +1,16 @@
-var exec = require('child_process').exec;
-var request = require('request');
-var fs = require('fs');
+const exec = require('child_process').exec;
+const request = require('request');
+const fs = require('fs');
 
 (function timer() {
     // TODO add Mac/Windows command
-    exec('import -window root -quality 50 screenshot.jpg', function () {
+    exec('import -window root -quality 50 screenshot.jpg', () => {
         fs.createReadStream('screenshot.jpg').pipe(request.post(process.argv[2] + '/screenshot'));
     });
-    exec('import -window root -resize 200 thumbnail.jpg', function () {
+    exec('import -window root -resize 200 thumbnail.jpg', () => {
         fs.createReadStream('thumbnail.jpg').pipe(request.post(process.argv[2] + '/thumbnail'));
     });
     setTimeout(timer, 10 * 60 * 1000); // once 10 minutes
 }());
 
-process.on('uncaughtException', function (err) {
-//     console.log('uncaughtException');
-});
+process.on('uncaughtException', err => console.log('uncaughtException'));
